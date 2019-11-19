5444f198507f40839826ecd3201b0708251687e0
Pkononsky <Pkononsky@yandex.ru>
2019-11-19 17:25:00 +0500

    переделано без генератора

--- lib.js
+++ lib.js
@@ -28,11 +28,11 @@ function getNextFriendToCheck(friends, friendsToCheck, visitedFriends) {
     return nextFriends;
 }
 
-function* bypassFriendsGraph(friends, filter, maxLevel = -1) {
+function bypassFriendsGraph(friends, filter, maxLevel = -1) {
     const bestFriends = getBestFriends(friends);
-
     let visitedFriends = [...bestFriends];
     let friendsToCheck = [...bestFriends];
+    let suitableFriends = [];
     let currentLevel = 0;
     while (friendsToCheck.length !== 0 && currentLevel !== maxLevel) {
         currentLevel++;
@@ -41,15 +41,14 @@ function* bypassFriendsGraph(friends, filter, maxLevel = -1) {
 
             return filter.isSuitable(friendObj);
         }).sort();
-        let suitableFriends = suitableFriendsName.map((friendName) => {
+        suitableFriends.push(...suitableFriendsName.map((friendName) => {
             return getFriendObjectByName(friends, friendName);
-        });
-        while (suitableFriends.length !== 0) {
-            yield suitableFriends.shift();
-        }
+        }));
         friendsToCheck = getNextFriendToCheck(friends, friendsToCheck, visitedFriends);
         visitedFriends.push(...friendsToCheck);
     }
+
+    return suitableFriends;
 }
 
 
@@ -63,20 +62,19 @@ function Iterator(friends, filter) {
     if (!(filter instanceof Filter)) {
         throw new TypeError(TYPE_ERROR_TEXT);
     }
-    this.bypassGenerator = bypassFriendsGraph(friends, filter);
-    this.generatorWorkResult = this.bypassGenerator.next();
+    this.suitableFriends = bypassFriendsGraph(friends, filter);
+    this.idDone = this.suitableFriends.length === 0;
 }
 
 Iterator.prototype = {
     next: function () {
-        let nextFriend = this.generatorWorkResult.value;
-        nextFriend = nextFriend === undefined ? null : this.generatorWorkResult.value;
-        this.generatorWorkResult = this.bypassGenerator.next();
+        let friend = this.suitableFriends.shift();
+        this.idDone = this.suitableFriends.length === 0;
 
-        return nextFriend;
+        return friend;
     },
     done: function () {
-        return this.generatorWorkResult.done;
+        return this.idDone;
     }
 };
 
@@ -92,8 +90,8 @@ function LimitedIterator(friends, filter, maxLevel) {
     if (!(filter instanceof Filter)) {
         throw new TypeError(TYPE_ERROR_TEXT);
     }
-    this.bypassGenerator = bypassFriendsGraph(friends, filter, maxLevel);
-    this.generatorWorkResult = this.bypassGenerator.next();
+    this.suitableFriends = bypassFriendsGraph(friends, filter, maxLevel);
+    this.idDone = this.suitableFriends.length === 0;
 }
 
 /**
